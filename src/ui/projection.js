c8fd9a4e1b703aaf8271427ca4d0c6da3827522d
Daryoush Ghoreishi <69098394+DaryoushShah@users.noreply.github.com>
2023-11-24 23:17:50 -0800

    Updated projection UI to state the number of years, months, and days till goal is reached

--- src/ui/projection.js
+++ src/ui/projection.js
@@ -44,11 +44,11 @@ const projection = () => {
             let totalCaloriesExpended = projectedBMR + user.info.exerciseSchedule[i];
             let weightLost = totalCalorieDiff/weightloss.CALORIES_PER_POUND;
             if(weightLost > 0) {
-              console.log(chalk.yellow(`${projectedDate.toLocaleDateString()}: `) + 'You expended ' + chalk.cyanBright(`${totalCaloriesExpended} calories`) + ' and consumed ' + chalk.redBright(`${user.info.eatingSchedule[i]} calories`) + ` resulting in ${weightLost} lbs lost`);
+              console.log(chalk.yellow(`${projectedDate.toLocaleDateString()} [${projectedWeight.toFixed(2)} lbs]: `) + 'You expended ' + chalk.cyanBright(`${totalCaloriesExpended} calories`) + ' and consumed ' + chalk.redBright(`${user.info.eatingSchedule[i]} calories`) + ` resulting in ${weightLost} lbs lost`);
             }else if(weightLost < 0){
-              console.log(chalk.yellow(`${projectedDate.toLocaleDateString()}: `) + 'You expended ' + chalk.cyanBright(`${totalCaloriesExpended} calories`) + ' and consumed ' + chalk.redBright(`${user.info.eatingSchedule[i]} calories`) + ` resulting in ${weightLost * -1} lbs gained`);
+              console.log(chalk.yellow(`${projectedDate.toLocaleDateString()} [${projectedWeight.toFixed(2)} lbs]: `) + 'You expended ' + chalk.cyanBright(`${totalCaloriesExpended} calories`) + ' and consumed ' + chalk.redBright(`${user.info.eatingSchedule[i]} calories`) + ` resulting in ${weightLost * -1} lbs gained`);
             }else{
-              console.log(chalk.yellow(`${projectedDate.toLocaleDateString()}: `) + 'You expended ' + chalk.cyanBright(`${totalCaloriesExpended} calories`) + ' and consumed ' + chalk.redBright(`${user.info.eatingSchedule[i]} calories`) + ` resulting in no change to your weight`);
+              console.log(chalk.yellow(`${projectedDate.toLocaleDateString()} [${projectedWeight.toFixed(2)} lbs]: `) + 'You expended ' + chalk.cyanBright(`${totalCaloriesExpended} calories`) + ' and consumed ' + chalk.redBright(`${user.info.eatingSchedule[i]} calories`) + ` resulting in no change to your weight`);
             }
           }
           projectedWeight -= totalCalorieDiff/weightloss.CALORIES_PER_POUND;
@@ -79,6 +79,25 @@ const projection = () => {
       }
       console.log(chalk.cyanBright(`You will lose around ${averageWeeklyWeightLoss} lbs per week`))
       console.log(chalk.cyanBright(`You will reach ${user.info.goalWeight} lbs by ${date.toLocaleDateString()}`));
+
+      /* Calculate length of time */
+      /* Insert time in days, months, years */
+      const monthsInYear = 12;
+      const daysInMonth = 30; // assuming an average of 30 days in a month for simplicity
+      
+      // Calculate time in days, months, and years
+      const years = Math.floor(daysPassed / 365);
+      const months = Math.floor((daysPassed % 365) / daysInMonth);
+      const days = daysPassed % daysInMonth;
+      
+      // Display the time
+      if (years > 0) {
+        console.log(chalk.cyanBright(`It will take ${years} year(s), ${months} month(s), and ${days} day(s)`));
+      } else if (months > 0) {
+        console.log(chalk.cyanBright(`It will take ${months} month(s) and ${days} day(s)`));
+      } else {
+        console.log(chalk.cyanBright(`It will take ${days} day(s)`));
+      }
     }
   })
 }
